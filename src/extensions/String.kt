1ce5dc58d6f34458b3da6017c625ba03a578e269
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2021-02-05 19:10:36 -0300

    Update String.kt

--- src/extensions/String.kt
+++ src/extensions/String.kt
@@ -1,3 +1,12 @@
+/*
+ * Create by Sungbin Ji on 2021. 1. 30.
+ * Copyright (c) 2021. Sungbin Ji. All rights reserved.
+ *
+ * AndroidUtils license is under the MIT license.
+ * SEE LICENSE : https://github.com/sungbin5304/AndroidUtils/blob/master/LICENSE
+ */
+
+
 package br.com.leotosin.androidutils.extensions
 
 import android.text.SpannableStringBuilder
