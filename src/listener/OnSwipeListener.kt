30ae9da16604c5779a1de1b5201ad06a9fdcf2b7
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2021-02-05 19:11:38 -0300

    Create OnSwipeListener.kt

---
+++ src/listener/OnSwipeListener.kt
@@ -0,0 +1,72 @@
+/*
+ * Create by Sungbin Ji on 2021. 1. 30.
+ * Copyright (c) 2021. Sungbin Ji. All rights reserved.
+ *
+ * AndroidUtils license is under the MIT license.
+ * SEE LICENSE : https://github.com/sungbin5304/AndroidUtils/blob/master/LICENSE
+ */
+
+package br.com.leotosin.androidutils.listener
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import kotlin.math.abs
+
+open class OnSwipeListener(context: Context) : View.OnTouchListener {
+    private val swipeThreshold = 100
+    private val swipeVelocityThreshold = 100
+    private val gestureDetector: GestureDetector
+
+    init {
+        gestureDetector = GestureDetector(context, GestureListener())
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouch(v: View, event: MotionEvent) = gestureDetector.onTouchEvent(event)
+
+    private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onDown(e: MotionEvent) = true
+        override fun onFling(
+            e1: MotionEvent,
+            e2: MotionEvent,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            var result = false
+            try {
+                val diffY = e2.y - e1.y
+                val diffX = e2.x - e1.x
+                if (abs(diffX) > abs(diffY)) {
+                    if (abs(diffX) > swipeThreshold &&
+                        abs(velocityX) > swipeVelocityThreshold
+                    ) {
+                        if (diffX > 0) {
+                            onSwipeLeftToRight()
+                        } else {
+                            onSwipeRightToLeft()
+                        }
+                        result = true
+                    }
+                } else if (abs(diffY) > swipeThreshold && abs(velocityY) > swipeVelocityThreshold) {
+                    if (diffY > 0) {
+                        onSwipeTopToBottom()
+                    } else {
+                        onSwipeBottomToTop()
+                    }
+                    result = true
+                }
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+            }
+            return result
+        }
+    }
+
+    open fun onSwipeLeftToRight() {}
+    open fun onSwipeRightToLeft() {}
+    open fun onSwipeBottomToTop() {}
+    open fun onSwipeTopToBottom() {}
+}
