c88a82319c945a988d75bf3cb7e09854a910c401
Leonardo B. Tosin <40045069+TheLe0@users.noreply.github.com>
2021-02-05 19:10:02 -0300

    String extension

---
+++ src/extensions/String.kt
@@ -0,0 +1,19 @@
+package br.com.leotosin.androidutils.extensions
+
+import android.text.SpannableStringBuilder
+import java.util.regex.Pattern
+
+fun String.replaceLast(findText: String, replaceText: String): String {
+    return if (contains(findText)) {
+        val lastIndex = lastIndexOf(findText)
+        val string = substring(0, lastIndex)
+        val string2 = substring((lastIndex + findText.length), this.length)
+        string + replaceText + string2
+    } else this
+}
+
+fun String.isUpperCase() = Pattern.matches("[A-Z]*$", this)
+
+fun String.isLowerCase() = Pattern.matches("[a-z]*$", this)
+
+fun String?.toEditable() = SpannableStringBuilder(this ?: "")
